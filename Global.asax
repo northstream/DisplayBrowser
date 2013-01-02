f75caf28aaa984131fe8c75dfafaf5ec845f0161
Barys Dzevenski <bdzevenski@emailtopia.com>
2013-01-02 16:34:51 -0500

    Test

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DisplayBrowser.Global" Language="C#" %>
---
+++ Mobile.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DetectBrowser
+{
+    public partial class Mobile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string u = Request.ServerVariables["HTTP_USER_AGENT"];
+            lblAgent.Text = "User Agent: " + u;
+        }
+    }
+}
